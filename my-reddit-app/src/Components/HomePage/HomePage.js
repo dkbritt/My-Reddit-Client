8a9582f978423ac6f4e129fa9e6534397498dcf1
Dbraxton6 <daishakb@gmail.com>
2025-02-28 18:21:18 -0800

    General homepage styling

--- my-reddit-app/src/Components/HomePage/HomePage.js
+++ my-reddit-app/src/Components/HomePage/HomePage.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { FaSearch, FaReddit } from 'react-icons/fa';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
         <div className='home'>
             <div className='banner'>
-                <img src='' alt="RedditLurker" id='logo' />
-                <p id='logoText'>RedditLurker</p>
-                {/* future search bar */}
+                <div className='bannerText'>
+                    <FaReddit className='icon reddit-logo' />
+                    <p id='logoText'><span id='logoText-reddit'>Reddit</span>Lurker</p>
+                </div>
+                <form className='searchBar'>
+                    <input type='text' placeholder='Search...' className='searchInput'/>
+                    <button className='searchButton' type='submit'>
+                        <FaSearch className='icon search-icon'/>
+                    </button>
+                </form>
             </div>
-            <div className='subreddits'>
-                <h1>SUBREDDITS</h1>
-                {/* subreddits list will go here */}
-            </div>
-            <div className='posts'>
-                <h1>POSTS</h1>
-                {/* posts will go here */}
+            <div className='pageContent'>
+                <div className='subreddits'>
+                    <h1>Subreddits</h1>
+                    {/* subreddits list will go here */}
+                </div>
+                <div className='posts'>
+                    <h1>Posts</h1>
+                    {/* posts will go here */}
+                </div>
             </div>
         </div>
     )
